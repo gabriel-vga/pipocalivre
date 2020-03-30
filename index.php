7cd0a540661cf30cf1d8bc3c1fe260b1e72dc9ea
SonicPliskin <50253718+SonicPliskin@users.noreply.github.com>
2020-03-30 19:36:36 -0300

    Arquivos do Projeto

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Pipoca Livre</title>
+        <meta name="description" content="Pipoca Livre, lar do cinema e da cultura pop"/>
+        <meta name="robots" content="index, follow"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="shortcut icon" href="img/favicon.png"/>
+        <link rel="stylesheet" href="css/style.css">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header class="main_header container">
+            <div class="content">
+                <img class="main_logo" title="Pipoca Livre" alt="[Pipoca Livre]" src="img/logo-pipocalivre.png"/>
+            <h1 class="fontzero">Pipoca Livre</h1>
+            <ul class="main_menu">
+                    <li><a href="#home" title="">Home</a></li>
+                    <li><a class="especial" href="#osite" title="Sobre o site Pipoca Livre">O Site</a></li>
+                    <li><a target="_blank" href="https://www.facebook.com/PipocaLivre/" title="Página Oficial Pipoca Livre">Facebook</a></li>
+					<li><a href="#criticas" title="Confira nossas últimas críticas">Críticas</a></li>
+					<li><a href="#lan" title="Confira os próximos lançamentos no cinema">Próx. Lançamentos</a></li>
+                    <li><a href="#contato" title="Contato com os adminstradores do site">Contato</a></li>
+                </ul>
+                <div class="clear"></div>
+            </div>
+        </header>
+        <main class="container">
+            <article class="destaque container">
+                <div class="content">
+                <header>
+                    <H1>Bem-Vindo(a) ao Pipoca Livre, o lar da cultura pop!</H1>
+                    <p class="tagline">Confira o trailer de Rogue One: Uma História Star Wars, o grande lançamento de 2016!</p>
+                    <div>
+                      <div class="fb-like" data-href="https://www.facebook.com/PipocaLivre/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div> 
+                      <div class="fb-share-button" data-href="https://www.facebook.com/PipocaLivre/" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.facebook.com/PipocaLivre%2F&amp;src=sdkpreparse">Compartilhar</a></div>
+                    </div>
+                </header>
+                    <div class="destaque_video">
+                        <div class="ratio">
+                            <iframe width="100%" class="media" src="https://www.youtube.com/embed/ySVDUw-c3PU" frameborder="0" allowfullscreen></iframe>
+						</div>
+                    </div>
+                    <div class="clear"></div>
+                </div>    
+            </article>
+            <section class="container novidades">
+                <div class="content">
+                    <header class="big_title">
+                    <h1>Navegue pelo nosso conteúdo</h1>
+                    <p class="tagline">Confira as principais novidades no cinema e na cultura pop!</p>
+                </header>
+                    <article class="novidades_item">
+                    <img title="CINEMA" alt="[CINEMA]" src="img/1.jpg"/>
+					<h1>CINEMA</h1>
+					<p>Confira nosso review sobre o polêmico Esquadrão Suícida, o mais recente filme da DC Comics para o cinema</p>
+                </article>
+                    <article class="novidades_item">
+                    <img title="MÚSICA" alt="[MÚSICA]" src="img/2.jpg"/>
+					<h1>MÚSICA</h1>
+					<p>Ouça a mais recente música de Lady Gaga e saiba mais sobre seu novo álbum</p>
+                </article>
+                    <article class="novidades_item">
+                    <img title="TELEVISÃO" alt="[TELEVISÃO]" src="img/3.jpg"/>
+					<h1>TELEVISÃO</h1>
+					<p>Leia as últimas notícias sobre a 7ª temporada de The Walking Dead</p>
+                </article>
+                    <article class="novidades_item">
+                    <img title="GAMES" alt="[GAMES]" src="img/4.jpg"/>
+					<h1>GAMES</h1>
+					<p>Confira nossa análise do retorno da franquia The King of Fighters, agora para o Playstation 4</p>
+                </article>
+                <div class="clear"></div>
+                </div>                    
+                <footer class="container novidades_footer">
+                    <div class="content">
+                    <h1>Confira as matérias e notícias anteriores</h1>
+                    <p class="tagline">Críticas, notícias, artigos e muito mais</p>
+                    <a class="btn" target="_blank" title="POSTAGENS ANTERIORES" href="#lastposts">POSTAGENS ANTERIORES</a>
+                    <div class="clear"></div>
+                    </div>
+                </footer>
+            </section>
+            <section class="container criticas">
+                <div class="content">
+                    <header class="big_title">
+                    <h1>CRÍTICAS</h1>
+                    <P class="tagline">Confira as últimas críticas feitas pelo site</P>
+                </header>
+                <div class="criticas_videos">
+                    <article>
+                    <img title="WATCHMEN" alt="[WATCHMEN]" src="img/5.jpg"/>
+			<h1>WATCHMEN</h1>
+			<p>Uma das melhores adaptações de todos os tempos</p>
+                    </article>
+                                        <article>
+                    <img title="ZOOTOPIA" alt="[ZOOTOPIA]" src="img/6.jpg"/>
+			<h1>ZOOTOPIA</h1>
+			<p>Mesmo superestimado, uma boa indicação para crianças</p>
+                    </article>
+                    <article>
+                    <img title="FARGO" alt="[FARGO]" src="img/7.jpg"/>
+			<h1>FARGO</h1>
+			<p>Um verdadeiro efeito dominó de erros</p>
+                    </article>
+
+                                        <article>
+                    <img title="HOW I MET YOUR MOTHER" alt="[HOW I MET YOUR MOTHER]" src="img/8.jpg"/>
+			<h1>HOW I MET YOUR MOTHER</h1>
+			<p>O que importa é a jornada</p>
+                    </article>
+                    
+                                        <article>
+                    <img title="BIOSHOCK INFINITE" alt="[BIOSHOCK INFINITE]" src="img/9.jpg"/>
+			<h1>BIOSHOCK INFINITE</h1>
+			<p>Uma obra prima genial em um gênero saturado</p>
+                    </article>
+                                        <article>
+                    <img title="STRANGER THINGS" alt="[STRANGER THINGS]" src="img/10.jpg" />
+			<h1>STRANGER THINGS</h1> 
+			<p>Uma homenagem aos anos 1980 de extrema qualidade</p>
+                    </article>                    
+                    
+                </div>
+                    <aside class="criticas_sidebar">
+                        <div class="border_bottom">
+                            <div class="content">
+                        <header>
+                        <h1 class="title">Comentários</h1>
+                    </header>
+                        <article class="criticas_sidebar_comment">
+                        <img title="Comentário de Neo1999" alt="[Comentário de Neo1999]" src="img/foto1.jpg"/>
+                        <h1><span class="fontzero">Comentários de </span> Neo1999</h1>
+                        <p>Página de ótima qualidade, com conteúdo diversificado e interessante.</p>
+                    </article>
+                    <article class="criticas_sidebar_comment">
+                        <img title="Comentário de Yellow_Umbrella15" alt="[Comentário de Yellow_Umbrella15]" src="img/foto2.jpg"/>
+                        <h1><span class="fontzero">Comentários de </span> Yellow_Umbrella15</h1>
+                        <p>Conteúdo muito bem variado, com um foco ótimo para séries de TV e música pop.</p>
+                    </article>
+                            <div class="clear"></div>
+                            </div>
+                        </div>       
+                </aside>
+                    <div class="clear"></div>
+                </div>    
+            </section>
+			
+			<section class="container estreias">
+                <div class="content">
+                    <header class="big_title">
+                    <h1>Confira os próximos lançamentos:</h1>
+                    <p class="tagline">Os filmes que estarão em cartaz nas próximas semanas</p>
+                </header>
+                    <article class="estreias_item">
+                    <img title="7 HOMENS E UM DESTINO" alt="[7 HOMENS E UM DESTINO]" src="img/p1.jpg"/>
+					<h1>7 HOMENS E UM DESTINO</h1>
+					<p>22/09/2016</p>
+                </article>
+                    <article class="estreias_item">
+                    <img title="THE NEON DEMON" alt="[THE NEON DEMON]" src="img/p2.jpg"/>
+					<h1>THE NEON DEMON</h1>
+					<p>29/09/2016</p>
+                </article>
+                    <article class="estreias_item">
+                    <img title="O LAR DAS CRIANÇAS PECULIARES" alt="[O LAR DAS CRIANÇAS PECULIARES]" src="img/p3.jpg"/>
+					<h1>O LAR DAS CRIANÇAS PECULIARES</h1>
+					<p>29/09/2016</p>
+                </article>
+                    <article class="estreias_item">
+                    <img title="ASSASSINO A PREÇO FIXO 2" alt="[ASSASSINO A PREÇO FIXO 2]" src="img/p4.jpg"/>
+					<h1>ASSASSINO A PREÇO FIXO 2</h1>
+					<p>06/10/2016</p>
+                </article>              
+            </section>
+			
+            <article class="container contato">
+                <div class="content">
+                    <header>
+                    <h1>Entre em Contato:</h1>
+                    <p class="tagline">Entre em contato com nossos redatores e administradores</p>
+                </header>
+                    <form class="contato_form" action="" method="post">
+                        <label class="medium">
+                        <span>Nome:</span>
+                        <input type="text" name="nome" title="Informe Seu Nome:" placeholder="Informe Seu Nome:" required/>
+                    </label>
+                        <label class="medium">
+                        <span>E-mail:</span>
+                        <input type="email" name="email" title="Informe Seu E-mail:" placeholder="Informe Seu E-mail:" required/>
+                    </label>
+                    <label>
+                        <span>Mensagem:</span>
+                        <textarea rows="3" name="mensagem" title="Deixe sua Mensagem:" placeholder="Deixe sua Menagem:"></textarea>
+                        
+                    </label>
+                        <img class="form_load" title="Aguarde, Enviando Contato!" alt="[Aguarde, Enviando Contato!]" src="img/ajax-loader.gif"/>
+                    <button class="btn">Enviar Contato</button>
+                </form>
+                    <div class="clear"></div>
+                </div>
+            </article>
+            
+            
+        </main>
+
+        <footer class="container main_footer">
+            <div class="content">
+                <h1 class="title">PIPOCA LIVRE</h1>
+            <nav>
+                <h1 class="subtitle">Pipoca Livre, lar do cinema e da cultura pop!</h1>
+                <ul class="main_footer_social">
+                    <li><a target="_blanck" title="Pipoca Livre - No Twitter" href="#twitter">Twitter</a></li>
+                    <li> | </li>
+                    <li><a target="_blanck" title="Pipoca Livre - No Facebook" href="https://www.facebook.com/PipocaLivre/">Facebook</a></li>
+                    <li> | </li>
+                    <li><a target="_blanck" title="Pipoca Livre - No YouTube" href="#youtube">YouTube</a></li>
+					<li> | </li>
+                    <li><a target="_blanck" title="Pipoca Livre - No Instagram" href="#instagram">Instagram</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+            </div>
+        </footer>
+        
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.7";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+    </body>
+</html>
